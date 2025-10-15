e411b80288101e0b9f12d615f724e2025c57017a
GamerStudentLearns <767475@newcollege.ac.uk>
2025-10-15 15:31:30 +0100

    added script to transition from level one to level two

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -5,5 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    
+    void OnTriggerEnter (Collider other)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level2");
+    }
 }
